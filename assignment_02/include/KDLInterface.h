7925c2d6a467422e2aa58d98ba9706d780b49400
Saugata Biswas <sau.biswas@gmail.com>
2014-10-14 18:35:05 +0200

    Homework2 submission

--- assignment_02/include/KDLInterface.h
+++ assignment_02/include/KDLInterface.h
@@ -12,6 +12,8 @@ class KDLInterface {
 
 private:
 	bool checkJointLimits(KDL::JntArray);
+	const double jointMax[] = {5.840139, 2.617989, -0.0157081, 3.42919, 5.641589};
+	const double jointMin[] = {0.01006921, 0.01006921, -5.0264, 0.0221391, 0.11062};
 
 public:
 	KDLInterface();
--- assignment_02/src/KDLInterface.cpp
+++ assignment_02/src/KDLInterface.cpp
@@ -8,15 +8,34 @@ KDLInterface::KDLInterface() {
 }
 
 bool KDLInterface::checkJointLimits(KDL::JntArray jointpositions){
-
+	for (int i = 0; i < joinpositions.rows(); i++){
+		if ((jointpositions[i] <= jointMax[i]) && (jointposition[i] >= jointMin[i]))
+			return false;
+	} 
 	return true;
 }
 
 
 bool KDLInterface::SetArmPosition(const KDL::JntArray &jointpositions){
-
-	//check for joint limits and command arm position
-	return false; //after implementation change it to true.
+	if(!KDLInterface::checkJointLimits(jointpositions))
+		return false;
+	
+	youbot::YouBotManipulator *youBotArm;
+	youBotArm = new youbot::YouBotManipulator("youbot-manipulator");
+	youBotArm->doJointCommutation();
+	youBotArm->calibrateManipulator();
+	
+	
+	
+	std::vector<youbot::JointAngleSetpoint> jointSetAngle;
+	jointSetAngle.resize(5);
+	
+	for (int i = 0; i < joinpositions.rows(); i++){
+		jointSetAngle[i].angle = jointpositions[i]*radian;
+	}
+	
+	youBotArm->setJointData(jointpositions);
+	return true;
 }
 
 KDLInterface::~KDLInterface() {
