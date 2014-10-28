d7164601f0a9751ebcb6fd74438c8fab8b28cdb5
Saugata Biswas <sau.biswas@gmail.com>
2014-10-28 18:02:19 +0100

    Homework4

--- assignment_02/include/KDLInterface.h
+++ assignment_02/include/KDLInterface.h
@@ -5,19 +5,25 @@
 #include <youbot/YouBotBase.hpp>
 #include <youbot/YouBotManipulator.hpp>
 #include <vector>
+#include <sys/timeb.h>
+
 
 namespace HardwareEncapsulation {
 
 class KDLInterface {
 
 private:
+	bool checkJointLimits();
 	bool checkJointLimits(KDL::JntArray);
 	const double jointMax[] = {5.840139, 2.617989, -0.0157081, 3.42919, 5.641589};
 	const double jointMin[] = {0.01006921, 0.01006921, -5.0264, 0.0221391, 0.11062};
+	const double torable_angular_dist = 0.01;
 
 public:
 	KDLInterface();
 	bool SetArmPosition(const KDL::JntArray &);
+	bool SetArmPositionWithVelocity(const KDL::JntArray &);
+	bool VelocityController(const KDL::JntArray &jointvelocities, int time_in_mills);
 	virtual ~KDLInterface();
 };
 
--- assignment_02/src/KDLInterface.cpp
+++ assignment_02/src/KDLInterface.cpp
@@ -2,8 +2,21 @@
 
 namespace HardwareEncapsulation {
 
+int KDLInterface::getMilliCount(){
+	timeb tb;
+	ftime(&tb);
+	int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+	return nCount;
+}
+
+
 KDLInterface::KDLInterface() {
 	youbot_hardware = RobotHardware::getInstance();
+	
+	youbot::YouBotManipulator *youBotArm;
+	youBotArm = new youbot::YouBotManipulator("youbot-manipulator");
+	youBotArm->doJointCommutation();
+	youBotArm->calibrateManipulator();
 
 }
 
@@ -16,25 +29,83 @@ bool KDLInterface::checkJointLimits(KDL::JntArray jointpositions){
 }
 
 
+
+bool KDLInterface::checkJointLimits(){
+	
+	double JointAngle;
+	youbot::JointSensedAngle angle;
+	
+	for(std::size_t i=0;i<5;i++){
+		youBotArm->getArmJoint(i+1).getData(angle);
+		JointAngle = (double)angle.angle.value();
+		if (!((JointAngle <= jointMax[i]) && (JointAngle >= jointMin[i])))
+			return false;
+	}
+	
+	return true;
+}
+
+
 bool KDLInterface::SetArmPosition(const KDL::JntArray &jointpositions){
 	if(!KDLInterface::checkJointLimits(jointpositions))
 		return false;
 	
-	youbot::YouBotManipulator *youBotArm;
-	youBotArm = new youbot::YouBotManipulator("youbot-manipulator");
-	youBotArm->doJointCommutation();
-	youBotArm->calibrateManipulator();
-	
-	
-	
 	std::vector<youbot::JointAngleSetpoint> jointSetAngle;
 	jointSetAngle.resize(5);
 	
-	for (int i = 0; i < joinpositions.rows(); i++){
+	for (int i = 0; i < jointpositions.rows(); i++){
 		jointSetAngle[i].angle = jointpositions[i]*radian;
 	}
 	
 	youBotArm->setJointData(jointpositions);
+	return true;
+}
+
+bool KDLInterface::SetArmPositionWithVelocity(const KDL::JntArray &jointvelocities){
+	if(!KDLInterface::checkJointLimits(jointvelocities))
+		return false;
+	
+	
+	std::vector<youbot::JointVelocitySetpoint> jointSetVelocity;
+	jointSetVelocity.resize(5);
+	bool slow_down = false;
+	
+	double JointAngle;
+	youbot::JointSensedAngle angle;
+	
+	for(std::size_t i=0;i<5;i++){
+		youBotArm->getArmJoint(i+1).getData(angle);
+		JointAngle = (double)angle.angle.value();
+		if(((jointMax[i] - JointAngle) < torable_angular_dist) || ((JointAngle - jointMin[i]) < torable_angular_dist)){
+			slow_down = true;
+		}
+			
+	}
+	
+	
+	for (int i = 0; i < jointvelocities.rows(); i++){
+		if(slow_down){
+			jointSetVelocity[i].angularVelocity = 0.01;
+		}else{
+			jointSetVelocity[i].angularVelocity = jointvelocities[i]*radian;
+		}
+	}
+	
+	youBotArm->setJointData(jointSetVelocity);
+	return true;
+}
+
+bool KDLInterface::VelocityController(const KDL::JntArray &jointvelocities, int time_in_mills){
+	int starting_time = KDLInterface::getMilliCount();
+	while(abs(KDLInterface::getMilliCount() - starting_time) < time_in_mills){
+		
+		
+		if(!KDLInterface::checkJointLimits())
+			return false;
+			
+			
+		KDLInterface::SetArmPositionWithVelocity(jointvelocities);
+	}
 	return true;
 }
 
